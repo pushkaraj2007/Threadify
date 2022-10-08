d3741a035f9073dd87ed781f1ec31fa71448961f
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2022-10-08 22:39:32 +0530

    Minor changes

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -30,7 +30,7 @@ const HomePage: NextPage = () => {
                                 </svg>
                             </div>
                             <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Manage Threads</h2>
-                            <p className="leading-relaxed text-base">Threadify helps you to manage your threads easily with it's super useful features.</p>
+                            <p className="leading-relaxed text-base">Threadify helps you to manage your threads easily with its super useful features.</p>
                         </div>
                     </div>
                     <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -75,7 +75,7 @@ const HomePage: NextPage = () => {
                                 </svg>
                             </div>
                             <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Secure</h2>
-                            <p className="leading-relaxed text-base">Threadify believes in security is the priority, That's why we never share your information with anyone else.</p>
+                            <p className="leading-relaxed text-base">Threadify believes in security is the priority, Thats why we never share your information with anyone else.</p>
                         </div>
                     </div>
                 </div>
