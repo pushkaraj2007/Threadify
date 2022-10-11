7b02e687c4fdd6c7bd689b9e99ee3d0fa16e309b
Vaibhav Mishra <89067345+vaibhavmishra4g@users.noreply.github.com>
2022-10-11 20:38:18 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: NextPage = () => {
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    <p className="text-[20px]">🧵</p>
+                    <img src="/logo.svg" className="h-12" alt="" />                  
                     <span className="ml-3 text-xl">Threadify</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 Threadify —
