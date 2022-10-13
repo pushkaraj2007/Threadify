bd9ba4f188b9731dae34b06386dcf14243ddaa56
stevenovak123 <stevenovak0802@gmail.com>
2022-10-13 21:40:15 +0530

    makes changes files to accomodate Layout.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {ThemeProvider} from "next-themes"
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
