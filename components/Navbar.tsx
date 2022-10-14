de577a1651dc7a26cf3dfd3b9aeac24a874d7855
Steve Dsouza <59828288+stevenovak123@users.noreply.github.com>
2022-10-14 19:13:15 +0530

    Layout component (#22)
    * created layout.tsx
    
    * makes changes files to accomodate Layout.tsx
    
    * added utility classes to fix theme bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,15 +11,15 @@ const Navbar: NextPage = () => {
     }
 
     return (
-        <header className="text-gray-600 body-font">
-            <div className="container mx-auto justify-between flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center dark:text-gray-300 text-gray-900 mb-4 md:mb-0">
+        <header className="text-gray-600 bg-gray-100 body-font dark:bg-gray-900">
+            <div className="container flex flex-col flex-wrap items-center justify-between p-5 mx-auto md:flex-row">
+                <a className="flex items-center mb-4 font-medium text-gray-900 title-font dark:text-gray-300 md:mb-0">
                     <Image width="40px" height="40px" src="/logo.svg" className="h-12 filter dark:invert" alt="" />
                     <span className="ml-3 text-xl">Threadify</span>
                 </a>
                 <div className="flex">
-                    <button className="mr-7" onClick={toggleTheme}> {theme === "light" ? <FaMoon className="text-gray-900 text-xl" /> : <FaSun className="text-gray-300 text-xl" />}</button>
-                    <button className="bg-blue-500 flex items-center transition-all hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login with Twitter
+                    <button className="mr-7" onClick={toggleTheme}> {theme === "light" ? <FaMoon className="text-xl text-gray-900" /> : <FaSun className="text-xl text-gray-300" />}</button>
+                    <button className="flex items-center px-4 py-2 font-bold text-white transition-all bg-blue-500 rounded hover:bg-blue-700">Login with Twitter
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from "../components/Navbar";
-import HomePage from "../components/HomePage";
-import Footer from "../components/Footer";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
+import HomePage from '../components/HomePage';
+import Footer from '../components/Footer';
 import Meta from '../components/Meta';
 
 const Home: NextPage = () => {
   return (
-    <div className='px-8 dark:bg-gray-900 bg-gray-100'>
-      <Meta title='Threadify' />
+    <div className="px-8 bg-gray-100 dark:bg-gray-900">
+      <Meta title="Threadify" />
       <main>
-        <Navbar />
         <HomePage />
-        <Footer />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
