cf6baae65578927a7a4a03926eff2794d2053e47
Given Suman <givensuman@gmail.com>
2022-12-14 19:29:47 -0600

    Give button name prop

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -54,7 +54,7 @@ const HomePage: NextPage = () => {
                         description="Threadify believes in security as its priority, that's why we never share your information with anyone else."
                     />
                 </div>
-                <button className="flex px-8 py-2 mx-auto mt-16 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600" onClick={()=> signIn('twitter', { callbackUrl: 'http://localhost:3000/api/twitter/user' })}>Get Started</button>
+                <button name="Get Started" className="flex px-8 py-2 mx-auto mt-16 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600" onClick={()=> signIn('twitter', { callbackUrl: 'http://localhost:3000/api/twitter/user' })}>Get Started</button>
             </div>
         </section>
     )
